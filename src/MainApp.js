66e45104ce6d65b4d7059bf624f3b01b7d480167
Daniel Xie <danielyxie2002@gmail.com>
2022-01-22 18:42:50 -0500

    Basics for setting project visibility

--- src/MainApp.js
+++ src/MainApp.js
@@ -1,32 +1,50 @@
 import React from 'react';
 import './MainApp.css'
 
-function Card(props) {
-    return (
-        <div className="card">
-            <span className="title">{props.project["name"]}</span>
-            <span className="subtitle">{props.project["created_at"]}</span>
-            <p>{props.project["description"]}</p>
-            <div className="footer">
-                <a href="https://github.com">Repository Link</a>
+class Card extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {editable: true, project: props.project}
+    }
+
+    render() {
+        const editable = this.state.editable;
+        const project = this.state.project;
+        const description = project["description"] == null ? '' : project["description"];
+        return (
+            <div className="card">
+                <span className="title">{project["name"]}</span>
+                <span className="subtitle">{project["created_at"].split("T")[0]}</span>
+                {editable
+                    ? <textarea defaultValue={description}></textarea>
+                    : <p>{description}</p>}
+                <div className="footer">
+                    <a href={project["html_url"]}>Repository Link</a>
+                    {editable && <button className="hide-project-button">Hide</button>}
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+    
+
 }
 
 export default class MainApp extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { userData: props.userData, repoData: props.repoData };
+        this.state = { userData: props.userData, 
+        projectData: props.repoData.map(project => [true, project])};
     }
 
     render() {
-        const repoData = this.state.repoData;
+        const userData = this.state.userData;
+        const projectData = this.state.projectData;
+        const visibleProjects = projectData.map(([visible, project]) => (visible && project));
         return (
             <div>
                 <div className="header">
                     <div className="pfp"></div>
-                    <h1>Username</h1>
+                    <h1>{userData["name"]}</h1>
                     <ul className="navbar">
                         <li><a href="https://google.com">About</a></li>
                         <li><a href="https://google.com">Projects</a></li>
@@ -34,7 +52,7 @@ export default class MainApp extends React.Component {
 
                 </div>
                 <div className="container">
-                    {repoData.map((project, index) =>
+                    {visibleProjects.map((project, index) =>
                         <Card project={project} key={index} />
                     )}
                 </div>
