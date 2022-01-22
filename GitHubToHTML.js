fd76c78cb5cfbd91d89ac9eb35159b8fd80bad21
fuzzyhappy <student.e.wang@gmail.com>
2022-01-22 17:26:35 -0500

    Moving over to polishing React components

--- GitHubToHTML.js
+++ GitHubToHTML.js
@@ -1,11 +1,32 @@
 const { Octokit } = require("@octokit/rest");
 const readline = require("readline");
 const util = require('util');
+const fs = require('fs');
+
+var html = `<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>RepoFolio</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="style.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+
+    <body>
+        <div class = "header">
+            <div class="pfp"></div>
+            <h1>Username</h1>
+            <ul class="navbar">
+                <li><a href="https://google.com">About</a></li>
+                <li><a href="https://google.com">Projects</a></li>
+            </ul>
+
+        </div>`;
 
 process.on('uncaughtException', function(err) {
     console.log(err.stack);
     throw err;
-  });
+});
 
 const octokit = new Octokit();
 const rl = readline.createInterface(process.stdin, process.stdout);
@@ -14,16 +35,24 @@ const question = util.promisify(rl.question).bind(rl);
 const getUsername = async () => { return await question("Enter GitHub username: "); }
 const getRepoData = async (uname) => { return await octokit.request("/users/" + uname +  "/repos"); }
 
-
 (async () => {
+
     try {
-        console.log("checkpoint")
+
 	    const uname = await getUsername();
-        console.log("checkpoint2")
-        console.log(uname);
         var repoData = await getRepoData(uname); 
         repoData = repoData["data"]; // is a list of dictionaries, e.g. repoData[0]["name"]
         console.log(repoData);
+
+        fs.mkdir("./out/", () => {
+        });
+
+        fs.writeFile("./out/index.html", "", () => {});
+
+        for (var i = 0; i < repoData.length; i++) {
+            console.log(repoData[i]["name"]);
+        }
+
     } catch (err) {
         console.log("FAILED");
     } finally {
